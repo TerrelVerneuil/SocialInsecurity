563c003b12237c4635de482f5021cc665da2a795
TerrelVerneuil <44128398+TerrelVerneuil@users.noreply.github.com>
2022-09-24 04:50:35 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,12 @@
-import os
-
+import os      
+from flask import Flask, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
 # contains application-wide configuration, and is loaded in __init__.py
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret' # TODO: Use this with wtforms
     DATABASE = 'database.db'
     UPLOAD_PATH = 'app/static/uploads'
-    ALLOWED_EXTENSIONS = {} # Might use this at some point, probably don't want people to upload any file type
+    ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'} # Might use this at some point, probably don't want people to upload any file type
+    app = Flask(__name__)
+    app.config['UPLOAD_PATH'] = UPLOAD_PATH
