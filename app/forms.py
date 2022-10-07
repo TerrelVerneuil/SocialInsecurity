97032d8e963624f5a1b182f52f2e2eb65fe12009
TerrelVerneuil <44128398+TerrelVerneuil@users.noreply.github.com>
2022-10-07 15:12:13 +0200

    Update forms.py
    revert

--- app/forms.py
+++ app/forms.py
@@ -1,47 +1,41 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, FormField, TextAreaField, FileField
 from wtforms.fields.html5 import DateField
-from wtforms.validators import DataRequired, Email, EqualTo, Length, InputRequired
+from wtforms.validators import DataRequired, Email, EqualTo, Length
 # defines all forms in the application, these will be instantiated by the template,
 # and the routes.py will read the values of the fields
 # TODO: Add validation, maybe use wtforms.validators??
 # TODO: There was some important security feature that wtforms provides, but I don't remember what; implement it
-from .__init__ import User
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', render_kw={'placeholder': 'Username'}, validators=[DataRequired(), Length(min=3, max=32)])
-    password = PasswordField('Password', render_kw={'placeholder': 'Password'}, validators=[DataRequired(), Length(min=4, max=20)])
+    username = StringField('Username', render_kw={'placeholder':'Username'}, validators= [DataRequired(), Length(min=3, max=32)])
+    password = PasswordField('Password',render_kw={'placeholder':'Password'},  validators=[DataRequired(), Length(min=4, max=20)])
     remember_me = BooleanField('Remember me') # TODO: It would be nice to have this feature implemented, probably by using cookies
     submit = SubmitField('Sign In')
 
 class RegisterForm(FlaskForm):
-    first_name = StringField('First Name', render_kw={'placeholder': 'First Name'}, validators=[DataRequired(), Length(min=1, max=20)])
-    last_name = StringField('Last Name', render_kw={'placeholder': 'Last Name'}, validators=[DataRequired(), Length(min=3, max=32)])
-    username = StringField('Username', render_kw={'placeholder': 'Username'}, validators=[DataRequired(), Length(min=3, max=32)])
-    password = PasswordField('Password', render_kw={'placeholder': 'Password'}, validators=[DataRequired(), Length(min=4, max=20)])
-    confirm_password = PasswordField('Confirm Password', render_kw={'placeholder': 'Confirm Password'}, validators=[DataRequired(), EqualTo('password', message='passwords must match')])
+    first_name = StringField('First Name',render_kw={'placeholder':'Username'}, validators=[DataRequired(), Length(min=1, max=20)])
+    last_name = StringField('Last Name',render_kw={'placeholder':'Last Name'}, validators=[DataRequired(), Length(min=1, max=20)])
+    username = StringField('Username',render_kw={'placeholder':'Username'}, validators= [DataRequired(), Length(min=3, max=32)])
+    password = PasswordField('Password',render_kw={'placeholder':'Password'}, validators=[DataRequired(), Length(min=4, max=20)])
+    confirm_password = PasswordField('Confirm Password',render_kw={'placeholder':'Confirm Password'}, validators=[DataRequired(), EqualTo('password',message='passwords must match')])
     submit = SubmitField('Sign Up')
-
-    def validate_username(self, username):
-        user = User.query.filter_by(username=username.data).first()
-        if user is not None:
-            raise ValidationError('Username already in use.')
 
 class IndexForm(FlaskForm):
     login = FormField(LoginForm)
     register = FormField(RegisterForm)
 
 class PostForm(FlaskForm):
-    content = TextAreaField('New Post', validators=[DataRequired(), Length(min=1, max=200)], render_kw={'placeholder': 'What are you thinking about?'})
+    content = TextAreaField('New Post', render_kw={'placeholder': 'What are you thinking about?'})
     image = FileField('Image')
     submit = SubmitField('Post')
-    
+
 class CommentsForm(FlaskForm):
-    comment = TextAreaField('New Comment', validators=[InputRequired(), Length(min=1, max=200)], render_kw={'placeholder': 'What do you have to say?'})
+    comment = TextAreaField('New Comment', render_kw={'placeholder': 'What do you have to say?'})
     submit = SubmitField('Comment')
 
 class FriendsForm(FlaskForm):
-    username = StringField('Friend\'s username', validators=[DataRequired(), Length(min=3, max=32)], render_kw={'placeholder': 'Username'})
+    username = StringField('Friend\'s username', render_kw={'placeholder': 'Username'})
     submit = SubmitField('Add Friend')
 
 class ProfileForm(FlaskForm):
