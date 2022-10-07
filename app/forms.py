db93fba1ca4e118c30eb55b623d3facac9b13d86
TerrelVerneuil <44128398+TerrelVerneuil@users.noreply.github.com>
2022-10-07 13:16:05 +0200

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -6,20 +6,26 @@ from wtforms.validators import DataRequired, Email, EqualTo, Length, InputRequir
 # and the routes.py will read the values of the fields
 # TODO: Add validation, maybe use wtforms.validators??
 # TODO: There was some important security feature that wtforms provides, but I don't remember what; implement it
+from .__init__ import User
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', render_kw={'placeholder':'Username'}, validators= [DataRequired(), Length(min=3, max=32)])
-    password = PasswordField('Password',render_kw={'placeholder':'Password'},  validators=[DataRequired(), Length(min=4, max=20)])
+    username = StringField('Username', render_kw={'placeholder': 'Username'}, validators=[DataRequired(), Length(min=3, max=32)])
+    password = PasswordField('Password', render_kw={'placeholder': 'Password'}, validators=[DataRequired(), Length(min=4, max=20)])
     remember_me = BooleanField('Remember me') # TODO: It would be nice to have this feature implemented, probably by using cookies
     submit = SubmitField('Sign In')
 
 class RegisterForm(FlaskForm):
-    first_name = StringField('First Name',render_kw={'placeholder':'Username'}, validators=[DataRequired(), Length(min=1, max=20)])
-    last_name = StringField('Last Name',render_kw={'placeholder':'Last Name'}, validators=[DataRequired(), Length(min=1, max=20)])
-    username = StringField('Username',render_kw={'placeholder':'Username'}, validators= [DataRequired(), Length(min=3, max=32)])
-    password = PasswordField('Password',render_kw={'placeholder':'Password'}, validators=[DataRequired(), Length(min=4, max=20)])
-    confirm_password = PasswordField('Confirm Password',render_kw={'placeholder':'Confirm Password'}, validators=[DataRequired(), EqualTo('password',message='passwords must match')])
+    first_name = StringField('First Name', render_kw={'placeholder': 'First Name'}, validators=[DataRequired(), Length(min=1, max=20)])
+    last_name = StringField('Last Name', render_kw={'placeholder': 'Last Name'}, validators=[DataRequired(), Length(min=3, max=32)])
+    username = StringField('Username', render_kw={'placeholder': 'Username'}, validators=[DataRequired(), Length(min=3, max=32)])
+    password = PasswordField('Password', render_kw={'placeholder': 'Password'}, validators=[DataRequired(), Length(min=4, max=20)])
+    confirm_password = PasswordField('Confirm Password', render_kw={'placeholder': 'Confirm Password'}, validators=[DataRequired(), EqualTo('password', message='passwords must match')])
     submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Username already in use.')
 
 class IndexForm(FlaskForm):
     login = FormField(LoginForm)
