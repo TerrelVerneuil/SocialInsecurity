5c841b1298bd05db250b1a7f68d6e2802fcd1bb6
TerrelVerneuil <44128398+TerrelVerneuil@users.noreply.github.com>
2022-10-10 08:32:09 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -23,13 +23,6 @@ login.login_view = 'index'
 class User(db.Model,UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(30), unique = True)
-    password = db.Column(db.String(30), nullable=False)
-    
-    def set_password(self, password):
-        self.password = generate_password_hash(password)
-        
-    def check_password(self, password):
-        return check_password_hash(self.password, password)
         
 db.create_all()
 
