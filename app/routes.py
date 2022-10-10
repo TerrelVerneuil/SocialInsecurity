ed5e216d10dd951460f73a5e2bbe192e58235607
TerrelVerneuil <44128398+TerrelVerneuil@users.noreply.github.com>
2022-10-10 08:33:06 +0200

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -26,8 +26,8 @@ def index():
             flash('Sorry, wrong password!')
 
     elif form.register.is_submitted() and form.register.submit.data:
-        user = query_db('SELECT * FROM Users WHERE username="{}";'.format(form.login.username.data), one=True)
-        if user is not None:
+        user2 = User.query.filter_by(username=form.register.username.data).first()
+        if  user2 is not None:
             flash('This Username is already in use')
         query_db('INSERT INTO Users (username, first_name, last_name, password) VALUES("{}", "{}", "{}", "{}");'.format(form.register.username.data, form.register.first_name.data,
          form.register.last_name.data, form.register.password.data))
