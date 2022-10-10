063c107416e43fc121cb74c2e7889e2dd86d7d0a
TerrelVerneuil <44128398+TerrelVerneuil@users.noreply.github.com>
2022-10-10 21:37:37 +0200

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -2,45 +2,45 @@ from flask import render_template, flash, redirect, url_for, request
 from app import app, query_db
 from app.__init__ import User, db
 from app.forms import IndexForm, PostForm, FriendsForm, ProfileForm, CommentsForm
-from flask_login import login_user, login_required, logout_user
 from datetime import datetime
 import os
+from flask_login import login_user, logout_user, login_required, current_user
+from werkzeug.utils import secure_filename
+from django.contrib.auth import get_user_model
 
 # this file contains all the different routes, and the logic for communicating with the database
-# home page/login/registration
 ALLOWED_EXTENSIONS = set(['mp4','txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+# home page/login/registration
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
     form = IndexForm()
 
-    if form.login.is_submitted() and form.login.submit.data:
+    if form.validate_on_submit() and form.login.submit.data:
         user = query_db('SELECT * FROM Users WHERE username="{}";'.format(form.login.username.data), one=True)
         if user == None:
             flash('Sorry, this user does not exist!')
         elif user['password'] == form.login.password.data:
-            user2 = User.query.filter_by(username=form.login.username.data).first()
-            login_user(user2)
             return redirect(url_for('stream', username=form.login.username.data))
         else:
             flash('Sorry, wrong password!')
 
-    elif form.register.is_submitted() and form.register.submit.data:
-        user2 = User.query.filter_by(username=form.register.username.data).first()
+    elif form.validate_on_submit() and form.register.submit.data:
         user = User.query.filter_by(username=form.register.username.data).first()
         if user == None:
             flash('Registered.')
         else:
             flash('Username in Use.')
+
+
         query_db('INSERT INTO Users (username, first_name, last_name, password) VALUES("{}", "{}", "{}", "{}");'.format(form.register.username.data, form.register.first_name.data,
-         form.register.last_name.data, form.register.password.data))
+            form.register.last_name.data, form.register.password.data))
         db.session.add(User(username=form.register.username.data))
-        #db.session.commit()
+       # db.session.commit()
         return redirect(url_for('index'))
     return render_template('index.html', title='Welcome', form=form)
 
 
-# content stream page
 def allowed_file(filename):
     return '.' in filename and \
             filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
@@ -49,31 +49,29 @@ def allowed_file(filename):
 def stream(username):
     form = PostForm()
     user = query_db('SELECT * FROM Users WHERE username="{}";'.format(username), one=True)
-
     if form.is_submitted() and request.method == 'POST':
         file = request.files['file']
         if form.content.data:
             query_db('INSERT INTO Posts (u_id, content, image, creation_time) VALUES({}, "{}", "{}", \'{}\');'.format(user['id'], form.content.data, file.filename, datetime.now()))
             return redirect(url_for('stream',username=username))
-        
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
             flash('File Successfully Uploaded')
-
         else:
             flash('Not a Compatible Format')
             return redirect(request.url)
 
 
+
         query_db('INSERT INTO Posts (u_id, content, image, creation_time) VALUES({}, "{}", "{}", \'{}\');'.format(user['id'], form.content.data, file.filename, datetime.now()))
         return redirect(url_for('stream', username=username))
+
     posts = query_db('SELECT p.*, u.*, (SELECT COUNT(*) FROM Comments WHERE p_id=p.id) AS cc FROM Posts AS p JOIN Users AS u ON u.id=p.u_id WHERE p.u_id IN (SELECT u_id FROM Friends WHERE f_id={0}) OR p.u_id IN (SELECT f_id FROM Friends WHERE u_id={0}) OR p.u_id={0} ORDER BY p.creation_time DESC;'.format(user['id']))
     return render_template('stream.html', title='Stream', username=username, form=form, posts=posts)
 
 # comment page for a given post and user.
 @app.route('/comments/<username>/<int:p_id>', methods=['GET', 'POST'])
-@login_required
 def comments(username, p_id):
     form = CommentsForm()
     if form.is_submitted():
@@ -84,9 +82,7 @@ def comments(username, p_id):
     all_comments = query_db('SELECT DISTINCT * FROM Comments AS c JOIN Users AS u ON c.u_id=u.id WHERE c.p_id={} ORDER BY c.creation_time DESC;'.format(p_id))
     return render_template('comments.html', title='Comments', username=username, form=form, post=post, comments=all_comments)
 
-# page for seeing and adding friends
 @app.route('/friends/<username>', methods=['GET', 'POST'])
-@login_required
 def friends(username):
     form = FriendsForm()
     user = query_db('SELECT * FROM Users WHERE username="{}";'.format(username), one=True)
@@ -96,13 +92,12 @@ def friends(username):
             flash('User does not exist')
         else:
             query_db('INSERT INTO Friends (u_id, f_id) VALUES({}, {});'.format(user['id'], friend['id']))
-    
+
     all_friends = query_db('SELECT * FROM Friends AS f JOIN Users as u ON f.f_id=u.id WHERE f.u_id={} AND f.f_id!={} ;'.format(user['id'], user['id']))
     return render_template('friends.html', title='Friends', username=username, friends=all_friends, form=form)
 
 # see and edit detailed profile information of a user
 @app.route('/profile/<username>', methods=['GET', 'POST'])
-@login_required
 def profile(username):
     form = ProfileForm()
     if form.is_submitted():
@@ -110,13 +105,11 @@ def profile(username):
             form.education.data, form.employment.data, form.music.data, form.movie.data, form.nationality.data, form.birthday.data, username
         ))
         return redirect(url_for('profile', username=username))
-    
+
     user = query_db('SELECT * FROM Users WHERE username="{}";'.format(username), one=True)
     return render_template('profile.html', title='profile', username=username, user=user, form=form)
 
-
-@app.route('/logout')
-@login_required
+@app.route('/logout', )
 def logout():
     logout_user()
     return redirect(url_for('index'))
