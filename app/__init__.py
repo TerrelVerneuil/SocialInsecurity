3fa4a93776d294737db5034e63a9dc26d4427999
TerrelVerneuil <44128398+TerrelVerneuil@users.noreply.github.com>
2022-10-10 07:54:44 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -26,7 +26,7 @@ class User(db.Model,UserMixin):
     password = db.Column(db.String(30), nullable=False)
     
     def set_password(self, password):
-        self.password - generate_password_hash(password)
+        self.password = generate_password_hash(password)
         
     def check_password(self, password):
         return check_password_hash(self.password, password)
